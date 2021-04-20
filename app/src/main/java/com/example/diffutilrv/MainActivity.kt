ca83b5fe59f331af4519b4541f73cca73a24e0ca
Kane Shih <kaneshih@yahoo-inc.com>
2021-04-20 14:19:15 +0800

    EmployeeListViewModel - create

--- app/src/main/java/com/example/diffutilrv/MainActivity.kt
+++ app/src/main/java/com/example/diffutilrv/MainActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diffutilrv.repo.DummyEmployeeDataUtils
 import com.example.diffutilrv.rvadapter.EmployeeRecyclerViewAdapter
+import com.example.diffutilrv.viewmodel.EmployeeListViewModel
+import com.example.diffutilrv.viewmodel.EmployeeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: EmployeeListViewModel
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: EmployeeRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViewModel()
+        initViews()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(
+            this,
+            EmployeeViewModelFactory()
+        ).get(EmployeeListViewModel::class.java)
+    }
+
+    private fun initViews() {
         adapter = EmployeeRecyclerViewAdapter(
             DummyEmployeeDataUtils.employeeListSortedByRole
         )
---
+++ app/src/main/java/com/example/diffutilrv/viewmodel/EmployeeListViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.diffutilrv.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class EmployeeListViewModel: ViewModel() {
+}
