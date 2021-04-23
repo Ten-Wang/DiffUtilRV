15bf571ad7ba4786dcec3811297e4e932739e118
Kane Shih <kaneshih@yahoo-inc.com>
2021-04-23 13:37:46 +0800

    ViewHolder newInstance() method

--- app/src/main/java/com/example/diffutilrv/rvadapter/viewholder/ViewHolder.kt
+++ app/src/main/java/com/example/diffutilrv/rvadapter/viewholder/ViewHolder.kt
@@ -1,6 +1,8 @@
 package com.example.diffutilrv.rvadapter.viewholder
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diffutilrv.R
@@ -10,6 +12,14 @@ class ViewHolder(
     itemView: View,
     clickListener: (adapterPosition: Int) -> Unit
 ) : RecyclerView.ViewHolder(itemView) {
+    companion object {
+        fun newInstance(parent: ViewGroup, clickListener: (adapterPosition: Int) -> Unit) =
+            ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false),
+                clickListener
+            )
+    }
+
     private val role: TextView = itemView.findViewById(R.id.employee_role)
     private val name: TextView = itemView.findViewById(R.id.employee_name)
 
